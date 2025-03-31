1d002ccb2755bd32ea81edcbb41b60892a0c407d
tuii-dev <tb@tuii.io>
2025-03-31 19:44:59 -0400

    Output issue

--- infrastructure/shared/ecr.tf
+++ infrastructure/shared/ecr.tf
@@ -118,10 +118,12 @@ resource "aws_ecr_repository_policy" "shared_ecr_policy" {
 
 output "ECR_REPOSITORY_URL" {
   value = aws_ecr_repository.container_repository.repository_url
+  sensitive = false
 }
 
 output "ECR_REPOSITORY_NAME" {
   value = aws_ecr_repository.container_repository.name
+  sensitive = false
 }
 
 output "CI_USER_ACCESS_KEY_ID" {
