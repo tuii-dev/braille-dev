2a0951c45f739786b5d322c4e04a21f6b10d9a91
tuii-dev <tb@tuii.io>
2025-04-16 11:41:58 -0400

    Optimize Docker build and improve PM2 startup script with better logging

--- packages/workflow/start.sh
+++ packages/workflow/start.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-LANGCHAIN_CALLBACKS_BACKGROUND=true yarn start:prod &
-sleep 2  # Give PM2 time to start
-pm2 logs --raw
+
+# Start PM2 services
+LANGCHAIN_CALLBACKS_BACKGROUND=true yarn start:prod
+
+# Give services a moment to initialize
+sleep 2
+
+# Show PM2 status
+echo "PM2 Status:"
+pm2 list
+
+# Keep the container running with the PM2 process
+exec pm2 logs
